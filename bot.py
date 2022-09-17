f3c7e1d58cc75ecbb4ef9e041d9768e0ec22540a
ctnivlac <thom8027@d.umn.edu>
2022-09-17 14:56:18 -0500

    Added main for the discord bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import nextcord
+import os
+from nextcord.ext import commands
+
+def main():
+   
+    intents = nextcord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print("The bot is ready to go!")
+
+    @bot.commands
+    async def ping(ctx):
+        await ctx.send("pong")
+
+    bot.run('MTAyMDQ0NTg1MjY1NDg5OTMwMQ.GPPiCY.PkwzyGjp6lbjN5JZNvOKwfBPDP4zZFmyZLLmdY')
+
+if __name__ == '__main__':
+    main()
+ 
